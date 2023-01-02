f05ea5b4cf08f959bdefb78eb63405dce6939eea
Amaranth01 <vanessa.nectoux@gmail.com>
2023-01-02 13:31:28 +0100

    Prepare the beginning

---
+++ View/base.html.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Links Handler</title>
+</head>
+<body>
+    <?php
+
+    if (isset($_SESSION['errors'])) {
+        $errors = $_SESSION['errors'];
+        unset($_SESSION['errors']);
+        ?>
+        <div class="message error">
+            <?= $errors ?>
+        </div> <?php
+    }
+    // Handling success messages.
+    if (isset($_SESSION['success'])) {
+        $success = $_SESSION['success'];
+        unset($_SESSION['success']);
+        ?>
+        <div class="message success">
+            <?= $success ?>
+        </div> <?php
+    }
+    ?>
+
+    <main class="container">
+        <?= $html ?>
+    </main>
+
+    <script src="https://kit.fontawesome.com/f33464c73a.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controller;
+class ErrorController extends AbstractController
+{
+    public function error404()
+    {
+        $this->render('error/404');
+    }
+}
